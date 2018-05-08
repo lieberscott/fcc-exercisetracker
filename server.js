8a6f0fcde2dc1f41619dee7e8bab2748ba3b45fe
Glitch (fcc-exercisetracker) <none>
2018-05-08 15:01:54 +0000

    👧😋 Checkpoint ./server.js:1015202/248

--- server.js
+++ server.js
@@ -12,11 +12,6 @@ app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-console.log("hello1");
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
@@ -47,6 +42,6 @@ app.use((err, req, res, next) => {
     .send(errMessage)
 })
 
-// const listener = app.listen(process.env.PORT || 3000, () => {
-//   console.log('Your app is listening on port ' + listener.address().port)
-// })
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
