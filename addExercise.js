db389305400524e54775c6b7da4c207e779ab3e6
Glitch (fcc-exercisetracker) <none>
2018-05-08 23:54:56 +0000

    🏦🎅 Checkpoint ./addExercise.js:1015202/134

--- addExercise.js
+++ addExercise.js
@@ -8,18 +8,17 @@ let addExercise = function(req, res) {
   let date = req.body.date;
   
   
-//   let findEditThenSave = function(personId, done) {
-//   let foodToAdd = 'hamburger';
+  let findEditThenSave = function(id, desc, dur, date, done) {
   
-//   Person.findById(personId, (err, data) => {
-//     if (err) { done(err) }
-//     data.favoriteFoods.push(foodToAdd);
-//     data.save((err, data) => {
-//       if (err) { done(err) }
-//       else { done(null, data) }
-//     });
-//   });
-// };
+  User.findById(id, (err, data) => {
+    if (err) { done(err) }
+    data.exercise.push(foodToAdd);
+    data.save((err, data) => {
+      if (err) { done(err) }
+      else { done(null, data) }
+    });
+  });
+};
   
   
   
