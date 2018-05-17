386428a8bfc2856dca19bfab2c1c1ab54fafded6
Glitch (fcc-exercisetracker) <none>
2018-05-17 22:10:36 +0000

    🎷🏮 Checkpoint ./lookup.js:1015202/2233

--- lookup.js
+++ lookup.js
@@ -44,63 +44,27 @@ let lookup = function(req, res) {
             username: 1,
             _id: 0
           }}
-      ])
+      ]).exec((err, data) => {
+          
+          data = {
+            ...data[0],
+            exercises: data[0].exercises.slice(0, limit)
+          };
     
-    
-    
-//     let user = Exerciser.aggregate([
-//     // Got info here: https://stackoverflow.com/questions/3985214/retrieve-only-the-queried-element-in-an-object-array-in-mongodb-collection
-//     { $match: { "username": id }},
-//     { $project: { // $project passes along the documents with the requested fields to the next stage in the pipeline
-//         exercises: { $filter: {
-//           input: "$exercises",
-//           as: "exercise",
-//           cond: { $and: [
-//             { $lte: [ "$$exercise.date", to ] },
-//             { $gte: [ "$$exercise.date", from ] },
-//           ]}
-//         }},
-//       username: 1,
-//       _id: 0
-//     }}
-// ])
-    
-    /*
-    let user = Exerciser.find({
-      "id": id,
-      "exercises": {
-        "$elemMatch":{ "date": { "$gte": from, "$lte": to } }
-      }
-        // "exercises": { "$slice": limit }
-    })
-      // { "exercises.date": { "$gte": from } },
-      // { "exercises.date": { "$lte": to } })
-    */
-        .exec((err, data) => {
-    
-    // let info = user.exercises.find( { "date": { "$gte": from, "$lte": to } } ).limit(limit).exec((err, data) => {
       if (err) {
         console.log(err);
         res.json({ Error: "Data not found" })
         return done(err);
       }
       else {
+        console.log(data);
         res.json(data);
         return done(null, data);
       }
     });
-    
-//     , (err, data) => {
-//       if (err) {
-//         res.json({ Error: "ID not found" });
-//         done(err);
-//       }
-      
-      
-//     });
+
   };
   findData(id, fromdate, todate, limit);
-  // findData(id, fromdate, todate, limit);
 
 };
 
