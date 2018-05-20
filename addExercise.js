a0cd68d288c0ff77f4ee16faa02aac6af466e321
Scott Lieber <WSox1235@aol.com>
2018-05-20 14:52:09 -0400

    wsBcBAABCAAQBQJbAcPZCRBK7hj4Ov3rIwAAdHIIABuLxFqQKrArW4l8HGlyDMb6  8UX03mzUi4lripv0qfxRGPA+Jnu36vfkzcMg2D+hz9Qk+7pitXkVXiKmSX8AOdEz  jRQEthp3croNO/PQ+TPmOOsdfB1S2WygxHIyqdksIdVpHJGCmFNp8VwCNXziEowY  ZSPhlWNs+segRetq8vfSokI91jsawHaLXkBH5Ah0aKU91KJTnF53y8eLLjhxn5tL  mZDTkmIyzvgnk1MdzxZ6zAqdSoRkCDIjxeHqTvLOvUrhO1pzZn0rwGqSkG/5CnXb  XLeR0PZXu4WZefOI0YNC5a7EXQFvuub4ij1kTllojGOUcLl4FhcJWTpFfWw4xW8=  =8J6r  -----END PGP SIGNATURE-----
    Added some error checking

--- addExercise.js
+++ addExercise.js
@@ -3,14 +3,23 @@ let Exerciser = require("./newUser.js").ExerciserModel;
 
 let addExercise = function(req, res) {
   let id = req.body.userId; // captures input field of form; "userId" here matches <input name="userId"> in index.html file
-  let desc = req.body.description;
-  let dur = req.body.duration;  
-  let arrdate = req.body.date.split("-");
-  let year = parseInt(arrdate[0]);
-  let month = parseInt(arrdate[1]) - 1;
-  let day = parseInt(arrdate[2]);
-  let date = new Date(year, month, day);
-  
+  let desc = req.body.description || "No Description given";
+  let dur = req.body.duration || 0;
+  let arrdate;
+  let year;
+  let month;
+  let day;
+  let date;
+  if (req.body.date) {
+    arrdate = req.body.date.split("-");
+    year = parseInt(arrdate[0]);
+    month = parseInt(arrdate[1]) - 1;
+    day = parseInt(arrdate[2]);
+    date = new Date(year, month, day);
+  }
+  else {
+    date = new Date();
+  }
   let findEditThenSave = function(id, desc, dur, date, done) {
   
     Exerciser.findById(id, (err, data) => {
